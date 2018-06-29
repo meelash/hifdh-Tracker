7f8c938fca59480ae94dcd9d66ccfb6d6a7b06d9
Saleem Abdul Hamid <meelash@gmail.com>
2018-06-29 19:12:24 +0500

    Initial Commit

---
+++ Hifdh Tracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hifdh Tracker
+//
+//  Created by سليم عبد الحميد on 6/29/18.
+//  Copyright © 2018 meelash. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Hifdh TrackerTests/Hifdh_TrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hifdh_TrackerTests.swift
+//  Hifdh TrackerTests
+//
+//  Created by سليم عبد الحميد on 6/29/18.
+//  Copyright © 2018 meelash. All rights reserved.
+//
+
+import XCTest
+@testable import Hifdh_Tracker
+
+class Hifdh_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
